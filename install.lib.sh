cc7bc2a8bf2b0e53148de34a073d6164a947e7c7
FrameTee29 <teeraphat123@hotmail.com>
2021-11-14 18:53:08 +0700

    - [chore] update lib

--- install.lib.sh
+++ install.lib.sh
@@ -2,7 +2,7 @@ echo "1. installing the core and supporting files "
 yarn add @nestjs/core @nestjs/common rxjs reflect-metadata 
 
 echo "2. Installing typeorm mysql"
-yarn add typeorm mysql @nestjs/typeorm
+yarn add typeorm mysql2 @nestjs/typeorm
 
 echo "3. Installing swagger"
 yarn add @nestjs/swagger swagger-ui-express
