7dc15f2e667bc829bd0deb2cc55b3f82e71401c3
FrameTee29 <teeraphat123@hotmail.com>
2021-11-14 18:40:26 +0700

    first commit

---
+++ install.lib.sh
@@ -0,0 +1,17 @@
+echo "1. installing the core and supporting files "
+yarn add @nestjs/core @nestjs/common rxjs reflect-metadata 
+
+echo "2. Installing typeorm mysql"
+yarn add typeorm mysql @nestjs/typeorm
+
+echo "3. Installing swagger"
+yarn add @nestjs/swagger swagger-ui-express
+
+echo "4. Installing config (dotenv)"
+yarn add @nestjs/config
+
+echo "5. Installing ValidationPipe"
+yarn add class-validator class-transformer
+
+echo "6. Installing @nestjs/axios"
+yarn add @nestjs/axios
